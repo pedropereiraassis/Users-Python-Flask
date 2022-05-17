7da0531d2870230ec160c9caf8c044407c457bf8
Pedro Pereira Assis <pedro.assis@fdte.io>
2022-05-17 19:24:58 -0300

    smal changes

--- config.py
+++ config.py
@@ -4,4 +4,4 @@ JWT_SECRET_KEY = '1234'
 # DATABASE_URL = 'postgresql://postgres:postgres@database:5432/database'
 
 # HEROKU
-DATABASE_URL = 'postgresql://noimjenfozamwq:293b917a7ccf051692ffc8f8c0c61cb049ef17203a12cd915ca680a0765d4d3e@ec2-54-165-184-219.compute-1.amazonaws.com:5432/dakicku6j0rd8e'
+DATABASE_URL = 'postgres://noimjenfozamwq:293b917a7ccf051692ffc8f8c0c61cb049ef17203a12cd915ca680a0765d4d3e@ec2-54-165-184-219.compute-1.amazonaws.com:5432/dakicku6j0rd8e'

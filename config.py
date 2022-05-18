956d03e4ced59010adbd7cd3e6a130aa6b0170d8
Pedro Pereira Assis <pedro.assis@fdte.io>
2022-05-18 12:00:21 -0300

    change database

--- config.py
+++ config.py
@@ -4,5 +4,5 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 JWT_SECRET_KEY='1234'
 FLASK_APP='app'
 FLASK_ENV='development'
-# DATABASE_URL = 'postgresql://wxbkjabflmmwxw:1546aea212b4db6dec657ad3293fc3dcd4484ec9776549cd92e10645f13d6e69@ec2-44-195-169-163.compute-1.amazonaws.com:5432/dcnjir3f5snd3n'
-DATABASE_URL = 'postgresql://postgres:postgres@database:5432/database'
+DATABASE_URL = 'postgresql://wxbkjabflmmwxw:1546aea212b4db6dec657ad3293fc3dcd4484ec9776549cd92e10645f13d6e69@ec2-44-195-169-163.compute-1.amazonaws.com:5432/dcnjir3f5snd3n'
+# DATABASE_URL = 'postgresql://postgres:postgres@database:5432/database'

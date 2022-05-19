76f48c45182b038eb980ca98f51acf390b37fbae
Pedro Pereira Assis <pedro.assis@fdte.io>
2022-05-19 14:11:53 -0300

    fixing database_url for heroku

--- app/__init__.py
+++ app/__init__.py
@@ -12,7 +12,7 @@ migrate = Migrate()
 def create_app():
 
   app = Flask(__name__)
-  app.config['SQLALCHEMY_DATABASE_URI'] = config('DATABASE_URL')
+  app.config['SQLALCHEMY_DATABASE_URI'] = config('DATABASE_URL').replace("postgres://", "postgresql://")
   app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
   app.config['JWT_SECRET_KEY'] = config('JWT_SECRET_KEY')
   app.config['JWT_BLACKLIST_ENABLED'] = True
